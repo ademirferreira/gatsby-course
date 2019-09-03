4acda76f12d7f53e77349c0f23ad6f869b0a6e9b
Ademir Ferreira <ademir.ae86@gmail.com>
2019-09-03 19:31:07 -0300

    Create about page and Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Ademir Ferreira</h1>
+    <h2>Programador Backend</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </div>
+)
+
+export default Profile
