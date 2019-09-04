ee6e79db3cb78796a4e3afdb084bcd641346b88a
Ademir Ferreira <ademir.ae86@gmail.com>
2019-09-03 22:18:45 -0300

    Refactoring Profile component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
+    position: 'Backend Developer',
     description: `A blog about frontend development and other cool stuff.`,
     author: `@whoisademir`,
   },
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Profile = () => (
-  <div className="Profile-wrapper">
-    <h1>Ademir Ferreira</h1>
-    <h2>Programador Backend</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-  </div>
-)
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, position, description },
+    },
+  } = useStaticQuery(graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          position
+          description            
+        }
+      }
+    }
+  `)
+  return (
+    <div className="Profile-wrapper">
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
+
 
 export default Profile
