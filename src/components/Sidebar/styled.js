0d0a713f1916e6708ec2701dce979b66f70f1141
Ademir Ferreira <ademir.ae86@gmail.com>
2019-09-05 22:04:35 -0300

    Create Sidebar Component

---
+++ src/components/Sidebar/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const SidebarWrapper = styled.aside`
+  align-items: center;
+  border-right: 1px solid #28444d;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  position: fixed;
+  text-align: center;
+  width: 20rem;
+`
