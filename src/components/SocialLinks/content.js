f5807db6b87633d7a43d5c68c49471fe3c9dc296
Ademir Ferreira <ademir.ae86@gmail.com>
2019-09-09 21:31:46 -0300

    Create SocialLinks component

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,25 @@
+const handle = 'johndoe'
+const links = [
+  {
+    label: 'Github',
+    url: `https://github.com/${handle}`
+  },
+  {
+    label: 'Twitter',
+    url: `https://twitter.com/${handle}`
+  },
+  {
+    label: 'Youtube',
+    url: `https://youtube.com/${handle}`
+  },
+  {
+    label: 'Instagram',
+    url: `https://instagram.com/${handle}`
+  },
+  {
+    label: 'Unsplash',
+    url: `https://unsplash.com/${handle}`
+  }
+]
+
+export default links
