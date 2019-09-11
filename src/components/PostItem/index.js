2d83577b2a5ac0a78c8dbd6def73370f3745d497
Ademir Ferreira <ademir.ae86@gmail.com>
2019-09-10 21:18:17 -0300

    Create PostItem component

---
+++ src/components/PostItem/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import * as S from './styled'
+
+const PostItem = () => (
+  <S.PostItemLink to="/slug">
+    <S.PostItemWrapper>
+      <S.PostItemTag background="#47650b">Misc</S.PostItemTag>
+      <S.PostItemInfo>
+        <S.PostItemDate>30 de Julho de 2019 - 4 min de leitura</S.PostItemDate>
+        <S.PostItemTitle>Diga Não ao Medium: tenha sua própria plataforma</S.PostItemTitle>
+        <S.PostItemDescription>
+          Algumas razões para você ter sua propria plataforma ao invés de soluções como o Medium.
+        </S.PostItemDescription>
+      </S.PostItemInfo>
+    </S.PostItemWrapper>
+  </S.PostItemLink>
+)
+
+export default PostItem
